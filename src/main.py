cae92edf06ec76030be5a811b7afd09b12eac555
root <root@localhost>
2025-08-29 15:54:23 +0600

    auto publish 1756461263549

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
-import math
-result = math.floor(3.4)
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+ 
+result = num1//num2
 print(result)
+
+print('==============')
+
+num3 = int(input('enter your 1st numb: '))
+num4 = int(input("enter your 2nd numb: "))
+
+result2 = num3//num4
+
+print(result2)
