12298d85dda8e2a6c43f03ce1c779037caf9d80c
root <root@localhost>
2025-08-30 07:46:11 +0600

    auto publish 1756518371074

--- src/main.py
+++ src/main.py
@@ -1,35 +1,34 @@
-def add(num1, num2):
-    return num1 + num2
+import string
+import random
  
-def subtract(num1, num2):
-    return num1 - num2
+def generate_password(size):
+    all_chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''
+    for char in range(size):
+        rand_char = random.choice(all_chars)
+        password = password + rand_char
+    return password
  
-def multiply(num1, num2):
-    return num1 * num2
- 
-def divide(num1, num2):
-    return num1 / num2
- 
-def modulo(num1, num2):
-    return num1 % num2
- 
-# Take input from the user
-num1 = int(input("Enter first number: "))
-operation = input("What you want to do(+, -, *, /, %):")
-num2 = int(input("Enter second number: "))
- 
-result = 0
-if operation == '+':
-    result = add(num1,num2)
-elif operation == '-':
-    result = subtract(num1,num2)
-elif operation == '*':
-    result = multiply(num1,num2)
-elif operation == '/':
-    result = divide(num1,num2)
-elif operation == '%':
-    result = modulo(num1,num2)
-else:
-    print("Please enter: +, -, *, / or %")
- 
-print(num1, operation, num2, '=', result)
+pass_len = int(input('How many characters in your password?'))
+new_password = generate_password(pass_len)
+print('Your new password: ', new_password)
+
+print('''
+______________________________________
+''')
+
+import string
+import random
+
+def g_p(a):
+  chars = string.ascii_letters + string.digits + string.punctuation
+  pw = ''
+  for i in range(a):
+    r_c = random.choice(chars)
+    pw = pw + r_c
+  return pw
+  
+p_l = int(input('pw length? '))
+n_pw = g_p(p_l)
+print('pw is: ', n_pw)
+
